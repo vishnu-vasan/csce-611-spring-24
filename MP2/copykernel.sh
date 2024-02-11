d7ba5020bffba6b50ddecd5d8d00609eb9276d48
vishnu-vasan <vasan2001@gmail.com>
2024-02-11 13:01:04 -0600

    after make

--- MP2/copykernel.sh
+++ MP2/copykernel.sh
@@ -1,13 +1,13 @@
 # Replace "/mnt/floppy" with the whatever directory is appropriate.
+
 # sudo mount -o loop dev_kernel_grub.img /mnt/floppy
 # sudo cp kernel.bin /mnt/floppy
 # sleep 1s
 # sudo umount /mnt/floppy
 
-
 mkdir ./floppy
 hdiutil attach dev_kernel_grub.img -mountpoint ./floppy
-cp kernel.elf ./floppy
+cp kernel.bin ./floppy
 sleep 1
 hdiutil detach ./floppy
 rmdir ./floppy
