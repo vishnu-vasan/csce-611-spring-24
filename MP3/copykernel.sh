e8b621a509c97f45ad0081a549aab3d74d9bc011
vishnu-vasan <vasan2001@gmail.com>
2024-02-24 23:46:36 -0600

    mp3 setup

---
+++ MP3/copykernel.sh
@@ -0,0 +1,11 @@
+# Replace "/mnt/floppy" with the whatever directory is appropriate.
+# sudo mount -o loop dev_kernel_grub.img /mnt/floppy
+# sudo cp kernel.bin /mnt/floppy
+# sleep 1s
+# sudo umount /mnt/floppy
+mkdir ./floppy
+hdiutil attach dev_kernel_grub.img -mountpoint ./floppy
+cp kernel.elf ./floppy
+sleep 1
+hdiutil detach ./floppy
+rmdir ./floppy
